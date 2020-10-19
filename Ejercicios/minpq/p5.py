b3568d5cd6a594638894b8e9becb610a4b238fb5
lef <le.florez602@uniandes.edu.co>
2020-10-19 12:14:31 -0500

    Cola de prioridad

--- Ejercicios/minpq/p5.py
+++ Ejercicios/minpq/p5.py
@@ -1,32 +1,64 @@
 import config
 
-from DISClib.ADT import indexminpq as pq
+# from DISClib.ADT import indexminpq as pq
+from DISClib.DataStructures import indexminpq as pq
 from DISClib.ADT import queue
 
 """
-Se tienen N logs de computadores ordenados por fecha y se quiere econtrar los 5 Errores mas recientes 
+Se tienen N logs de computadores ordenados por fecha y se quiere econtrar los 2 Errores mas recientes 
 """
 
-def greater(key1, entry):
-    if key1 == entry['key']:
+logs = [
+    [
+        {'t': 1, 'm': 'Error'},
+        {'t': 4, 'm': 'Error'},
+        {'t': 7, 'm': 'm'},
+    ],
+    [
+        {'t': 2, 'm': 'm'},
+        {'t': 5, 'm': 'Error'},
+        {'t': 8, 'm': 'm'},
+    ],
+    [
+        {'t': 3, 'm': 'm'},
+        {'t': 6, 'm': 'Error'},
+        {'t': 9, 'm': 'm'},
+    ]
+]
+
+
+def greater(k1, k2):
+    if k1['t'] == k2['t']:
         return 0
-    elif key1 < entry['key']:
+    elif k1['t'] > k2['t']:
         return -1
     else:
         return 1
 
-iminpq = pq.newIndexMinPQ(greater)
 
-iminpq = pq.insert(iminpq, 'A', 300)
-iminpq = pq.insert(iminpq, 'B', 101)
-iminpq = pq.insert(iminpq, 'C', 1000)
-iminpq = pq.insert(iminpq, 'D', 500)
 
-print(pq.delMin(iminpq))
+iminpq = pq.newIndexMinPQ(len(logs), greater)
 
-pq.decreaseKey(iminpq, 'D', 200)
-pq.increaseKey(iminpq, 'A', 202)
+for i, log in enumerate(logs):
+    pq.insert(iminpq, i, log.pop())
 
-print(pq.delMin(iminpq))
+
+num_err = 0
+
+while not pq.isEmpty(iminpq):
+    minLog = pq.minKey(iminpq)
+    minI = pq.delMin(iminpq)
+
+    if len(logs[minI]) > 0:
+        log = logs[minI].pop()
+        pq.insert(iminpq, minI, log)
+
+    if minLog['m'] == 'Error':
+        print(minLog)
+        num_err = num_err + 1
+        if num_err == 2:
+            break
+
+
 
 
